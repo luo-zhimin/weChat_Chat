22e42caa3b14d99cda6ce1b95a8098b3358adceb
镜像 <68786940+luo-zhimin@users.noreply.github.com>
2022-08-25 21:03:05 +0800

    Initial commit

---
+++ src/main/java/com/wechat/cn/WeChatChatApplication.java
@@ -0,0 +1,13 @@
+package com.wechat.cn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeChatChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeChatChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wechat/cn/WeChatChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wechat.cn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeChatChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
